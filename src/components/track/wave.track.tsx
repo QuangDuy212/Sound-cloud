9d5c5a71fbd349bffa48173a2b821c56da186d24
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-06-02 22:01:00 +0700

    video113

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,32 +1,11 @@
 'use client'
 
 import { useEffect, useRef, useState, useMemo } from "react";
-import WaveSurfer from "wavesurfer.js";
+import WaveSurfer, { WaveSurferOptions } from "wavesurfer.js";
 import { useSearchParams } from 'next/navigation';
+import { useWavesurfer } from "@/utils/customHook";
 
-// WaveSurfer hook
-const useWavesurfer = (containerRef: any, options: any) => {
-    const [wavesurfer, setWavesurfer] = useState<any>(null)
 
-    // Initialize wavesurfer when the container mounts
-    // or any of the props change
-    useEffect(() => {
-        if (!containerRef.current) return
-
-        const ws = WaveSurfer.create({
-            ...options,
-            container: containerRef.current,
-        })
-
-        setWavesurfer(ws)
-
-        return () => {
-            ws.destroy()
-        }
-    }, [options, containerRef])
-
-    return wavesurfer
-}
 const WaveTrack = () => {
     const searchParams = useSearchParams()
     const fileName = searchParams.get('audio');
@@ -37,32 +16,12 @@ const WaveTrack = () => {
             waveColor: 'rgb(200, 0, 200)',
             progressColor: 'rgb(100, 0, 100)',
             url: `/api?audio=${fileName}`,
+            container: containerRef.current,
         }
     }, []);
 
-    // const options = {
-    //     waveColor: 'rgb(200, 0, 200)',
-    //     progressColor: 'rgb(100, 0, 100)',
-    //     url: `/api?audio=${fileName}`,
-    // }
 
     const wavesurfer = useWavesurfer(containerRef, optionsMemo);
-
-
-    // useEffect(() => {
-    //     if (containerRef.current) {
-    //         const wavesurfer = WaveSurfer.create({
-    //             container: containerRef.current,
-    //             waveColor: 'rgb(200, 0, 200)',
-    //             progressColor: 'rgb(100, 0, 100)',
-    //             url: `/api?audio=${fileName}`,
-    //         })
-
-    //         return () => {
-    //             wavesurfer.destroy()
-    //         }
-    //     }
-    // }, [])
 
     return (
         <div ref={containerRef}>
