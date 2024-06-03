a3d7864cc92705784dfdabd08db357dd9f00b31b
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-06-03 15:21:58 +0700

    video119

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -57,6 +57,11 @@ const WaveTrack = () => {
         setIsPlaying(false);
         const timeEl = document.querySelector('#time')!
         const durationEl = document.querySelector('#duration')!//jquery
+
+        const hover = document.querySelector('#hover')!;
+        const waveform = containerRef.current!;
+        //@ts-ignore
+        waveform.addEventListener('pointermove', (e) => (hover.style.width = `${e.offsetX}px`))
         const subscription = [
             wavesurfer.on('play', () => setIsPlaying(true)),
             wavesurfer.on('pause', () => setIsPlaying(false)),
@@ -86,6 +91,7 @@ const WaveTrack = () => {
             <div ref={containerRef} className="wave-form-container">
                 <div id="time">0:00</div>
                 <div id="duration">0:00</div>
+                <div id="hover"></div>
             </div>
             <button onClick={() => onPlayPause()}>
                 {isPlaying ? "Pause" : "Play"}
