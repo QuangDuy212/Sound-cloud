cf7408c9e0470ce710a6c1c94f9b511cfbadecf8
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-06-16 09:32:23 +0700

    video155

--- src/app/(user)/profile/[slug]/page.tsx
+++ src/app/(user)/profile/[slug]/page.tsx
@@ -1,8 +1,33 @@
 
-const ProfilePage = ({ params }: { params: { slug: string } }) => {
+import { sendRequest } from "@/utils/api";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import ProfileTrack from "@/components/track/profile.tracks";
+import { Container } from "@mui/material";
+
+const ProfilePage = async ({ params }: { params: { slug: string } }) => {
+    const tracks = await sendRequest<IBackendRes<IModelPaginate<ITrackTop>>>({
+        url: `http://localhost:8000/api/v1/tracks/users?current=1&pageSize=10`,
+        method: "POST",
+        body: {
+            id: params.slug,
+        },
+    });
     return (
         <>
-            profile text slug
+            <Container sx={{ marginTop: "100px" }}>
+                <Box sx={{ flexGrow: 1, }}>
+                    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                        {tracks?.data && tracks?.data?.result.map((item, index) => {
+                            return (
+                                <Grid item xs={6} key={index}>
+                                    <ProfileTrack data={item} />
+                                </Grid>
+                            )
+                        })}
+                    </Grid>
+                </Box>
+            </Container>
         </>
     )
 }
