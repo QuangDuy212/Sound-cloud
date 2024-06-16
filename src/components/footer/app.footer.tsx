86ec8c382110ba87c554a794f75a3bcefa0b86e7
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-06-16 09:47:36 +0700

    video156

--- src/components/footer/app.footer.tsx
+++ src/components/footer/app.footer.tsx
@@ -14,28 +14,33 @@ const AppFooter = () => {
     return (
         <>
             <AppBar position="fixed"
-                sx={{ top: 'auto', bottom: 0, background: "#f2f2f2" }}
+                sx={{ top: 'auto', bottom: 0, background: "#f2f2f2", marginTop: "50px" }}
             >
                 <Container sx={{ display: "flex" }}>
                     <Grid container
                         alignItems="center"
                     >
-                        <Grid item xs={10}>
+                        <Grid item xs={10} sx={{
+                            ".rhap_main": {
+                                gap: "30px"
+                            }
+                        }}>
                             <AudioPlayer
-                                src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/hoidanit.mp3`}
+                                src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/24H-1718437193005.mp3`}
                                 volume={0.5}
                                 style={{
                                     boxShadow: "unset",
                                     background: '#f2f2f2'
                                 }}
+                                layout="horizontal-reverse"
                             />
                         </Grid>
                         <Grid item xs={2}>
                             <div className="info"
                                 style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}
                             >
-                                <div style={{ color: "#ccc", fontSize: "15px" }}>Quang Duy</div>
-                                <div style={{ color: "#333" }}>Grow up</div>
+                                <div style={{ color: "#ccc", fontSize: "15px" }}>LyLy</div>
+                                <div style={{ color: "#333" }}>24H</div>
                             </div>
                         </Grid>
                     </Grid>
